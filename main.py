91837a2259783a56a21a9a2fb5b9bc472763a340
Sam Fuller <spfuller01@gmail.com>
2021-02-14 20:05:05 -0600

    second commit

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
-import requests, os, discord, logging
+import requests, os, discord, sys
+from discord.ext import commands
+from geopy.geocoders import Nominatim
+from functools import partial
 from dotenv import load_dotenv
 load_dotenv()
 
-# print(data['weather'][0]['main'])
 
-logging.basicConfig(level=logging.INFO)
+# Logging if in dev mode
+if len(sys.argv) > 1 and sys.argv[1] == 'dev':
+    import logging
+    logging.basicConfig(level=logging.INFO)
 
-
-class WeatherBot(discord.Client):
+class WeatherBot(commands.Bot):
     async def on_ready(self):
+        await self.change_presence(status=discord.Status.online, activity=discord.Game('cloud watching'))
         print('Logged in as {0}!'.format(self.user))
 
-    def get_weather(self, query, units):
-        weather_api_key = os.getenv('API_KEY')
-        r = requests.get('https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units={2}'.format(query, weather_api_key, units))
-        return r.json()
+def geocoder(location):
+    geolocator = Nominatim(user_agent='WeatherBot - Discord bot for checking weather around the world.')
+    geocode = partial(geolocator.geocode, language='en')
+    return geocode(location)
 
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
+def get_weather(location, units):
+    weather_api_key = os.getenv('API_KEY')
+    location_lat = geocoder(location).latitude
+    location_lon = geocoder(location).longitude
+    api_url = f'https://api.openweathermap.org/data/2.5/onecall?lat={location_lat}&lon={location_lon}&exclude=minutely,hourly,alerts&appid={weather_api_key}&units={units}'
+    r = requests.get(api_url)
+    return r.json()
 
-        if message.content.startswith('!weather'):
-            # try:
-            args = message.content.split(' ')
-            unit = args[2] if len(args) > 2 else 'imperial'
-            data = self.get_weather(args[1], unit)
-            await message.channel.send('Current weather in {0} is `{1}` with a temperature of {2} degrees. It feels like {3} degrees though.'.format(args[1], data['weather'][0]['main'], data['main']['temp'], data['main']['feels_like']))
-        # except:
-                # await message.channel.send('Getting weather data failed.')
+wb = WeatherBot(command_prefix=os.getenv('PREFIX'))
 
-client = WeatherBot()
-client.run(os.getenv('TOKEN'))
+@wb.command()
+async def cweather(ctx, location, units='imperial'): 
+    data = get_weather(location, units)
+    await ctx.send(
+        'Current weather is `{0}` with a temp of {1}. It feels like {2}.'
+        .format(data['current']['weather'][0]['description'], data['current']['temp'], data['current']['feels_like'])
+    )
+
+@wb.command()
+async def dweather(ctx, location, units='imperial'):
+    await ctx.send('Daily weather feature coming soon!')
+
+@wb.command()
+async def help(ctx):
+    await ctx.send('Help message coming soon. for now just use $cweather. locations can be addresses (wraped in quotation marks!!!), cities, or zip codes.')
+
+wb.run(os.getenv('TOKEN'))
