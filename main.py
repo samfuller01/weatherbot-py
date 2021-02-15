f2dee6b08ab73a14c17b201a8ae53dd1707e8b56
Sam Fuller <spfuller01@gmail.com>
2021-02-14 23:13:03 -0600

    initial launch

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'dev':
 
 class WeatherBot(commands.Bot):
     async def on_ready(self):
-        await self.change_presence(status=discord.Status.online, activity=discord.Game('cloud watching'))
+        await self.change_presence(status=discord.Status.online, activity=discord.Game('cloud watching ($help)'))
         print('Logged in as {0}!'.format(self.user))
 
 def geocoder(location):
@@ -23,8 +23,9 @@ def geocoder(location):
 
 def get_weather(location, units):
     weather_api_key = os.getenv('API_KEY')
-    location_lat = geocoder(location).latitude
-    location_lon = geocoder(location).longitude
+    geocoded_location = geocoder(location)
+    location_lat = geocoded_location.latitude
+    location_lon = geocoded_location.longitude
     api_url = f'https://api.openweathermap.org/data/2.5/onecall?lat={location_lat}&lon={location_lon}&exclude=minutely,hourly,alerts&appid={weather_api_key}&units={units}'
     r = requests.get(api_url)
     return r.json()
@@ -33,6 +34,7 @@ wb = WeatherBot(command_prefix=os.getenv('PREFIX'))
 
 @wb.command()
 async def cweather(ctx, location, units='imperial'): 
+    """Gets current weather (wrap double quotes around location if the location has spaces in it)"""
     data = get_weather(location, units)
     await ctx.send(
         'Current weather is `{0}` with a temp of {1}. It feels like {2}.'
@@ -41,10 +43,18 @@ async def cweather(ctx, location, units='imperial'):
 
 @wb.command()
 async def dweather(ctx, location, units='imperial'):
-    await ctx.send('Daily weather feature coming soon!')
-
-@wb.command()
-async def help(ctx):
-    await ctx.send('Help message coming soon. for now just use $cweather. locations can be addresses (wraped in quotation marks!!!), cities, or zip codes.')
+    """Gets daily weather (wrap double quotes around location if the location has spaces in it)""" 
+    data = get_weather(location, units)
+    temp_high = []
+    temp_low = []
+    weather = []
+    for i in data['daily']:
+        temp_high.append(i['temp']['max']) 
+        temp_low.append(i['temp']['min']) 
+        weather.append(i['weather'][0]['description'])
+    await ctx.send(
+        'Over the next 8 days the weather is {0}. The high for each day is {1} and the low for each day is {2}.'
+        .format(', '.join(weather), ', '.join(map(str, temp_high)), ', '.join(map(str, temp_low)))
+    )
 
 wb.run(os.getenv('TOKEN'))
